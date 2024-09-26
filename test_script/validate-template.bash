513ee3e75f6a728a218489514a4340e18748f2f1
dhem <>
2024-09-26 19:23:24 +0800

    240926

--- test_script/validate-template.bash
+++ test_script/validate-template.bash
@@ -25,7 +25,7 @@ export NVIDIA_VISIBLE_DEVICES=1
 export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=power.draw,index --format=csv,nounits,noheader | sort -n | head -1 | awk '{ print $NF }')
 
 ## user's own commands below
-~/anaconda3/envs/pyscf/bin/python test.py -dl -0.45 0.45 10 -b cc-pVDZ --extend_atom 0 --extend_xyz 0 --load CHECKPOINT --name_mol benzene hexane --precision float32 --load_epoch -1 >log/CHECKPOINT.out
+~/anaconda3/envs/pyscf/bin/python test.py -dl -0.5 0.5 11 -b cc-pVDZ --extend_atom 0-1 --extend_xyz 0 --load CHECKPOINT --name_mol hexane octane --precision float32 --load_epoch -1 >log/CHECKPOINT.out
 #
 # ~/anaconda3/envs/pyscf/bin/python test.py -dl -0.95 0.95 20 -b cc-pCVTZ --extend_atom 0 --extend_xyz 0 --load CHECKPOINT --name_mol benzene cyclopentane isopentane pentane butane butyne isobutane butadiene propane propylene propyne allene methane ethane ethylene acetylene cyclopropene cyclopropane --input_size INPUT_SIZE --hidden_size HIDDEN_SIZE --output_size OUTPUT_SIZE --residual RESIDUAL --num_layer NUM_LAYER --precision float32 --load_epoch -1 >log/CHECKPOINT.out
 #
