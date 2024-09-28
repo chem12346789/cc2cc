375fe093f2e458dec49dfcacec0f0482172fe11e
czh <pb142060@mail.ustc.edu.cn>
2024-09-28 23:47:53 +0800

    240928

---
+++ train_script/train-template-krr.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#slurm options
+#SBATCH -n 24
+#SBATCH --mem 50G
+#SBATCH -p cpu
+#SBATCH -J train-ccdft-BASH_GAMMA-BASH_ALPHA
+#SBATCH -o log/%j.log
+
+## user's own commands below
+export OMP_NUM_THREADS=12
+export MKL_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
+
+export PYTHONPATH=~/python:$PYTHONPATH
+export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
+
+~/anaconda3/envs/pyscf/bin/python krr.py --gamma BASH_GAMMA --alpha BASH_ALPHA
