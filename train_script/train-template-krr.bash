6ab86876454f45aff9d26a75c0793b8299228d12
dhem <>
2024-11-04 19:14:57 +0800

    241103

--- train_script/train-template-krr.bash
+++ train_script/train-template-krr.bash
@@ -2,7 +2,7 @@
 
 #slurm options
 #SBATCH -n 24
-#SBATCH -p cpu
+#SBATCH -p gpu
 #SBATCH -J train-ccdft-BASH_GAMMA-BASH_ALPHA
 #SBATCH -o log/BASH_GAMMA-BASH_ALPHA.log
 
@@ -15,4 +15,4 @@ export NUMBA_NUM_THREADS=12
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/envs/pyscf/bin/python krr_fit.py --gamma BASH_GAMMA --alpha BASH_ALPHA --distance_list -0.5 -0.5 1 --molecular_list methane ethane propane
+~/anaconda3/envs/pyscf/bin/python view_data.py --gamma BASH_GAMMA --alpha BASH_ALPHA --distance_list -0.5 -0.5 1 --molecular_list methane ethane propane
