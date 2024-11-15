faccc470af4b9a668cef8354bb4b2d4723caae46
dhem <>
2024-11-15 12:00:14 +0800

    241115

--- train_script/train-template-krr.bash
+++ train_script/train-template-krr.bash
@@ -12,7 +12,8 @@ export MKL_NUM_THREADS=12
 export OPENBLAS_NUM_THREADS=12
 export NUMBA_NUM_THREADS=12
 export DFT2CC_CUBE_USE=5
+export DFT2CC_PERIOD=1
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/envs/pyscf/bin/python view_data.py --gamma BASH_GAMMA --alpha BASH_ALPHA --distance_list -0.5 -0.5 1 --molecular_list ethane
+~/anaconda3/envs/pyscf/bin/python krr_view.py --gamma BASH_GAMMA --alpha BASH_ALPHA --distance_list -0.5 0.5 11 --molecular_list methane ethane ethylene acetylene
