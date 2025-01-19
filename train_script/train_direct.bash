a4e12f62a03cbe67b43b7007d67c58b886674301
czh <pb142060@mail.ustc.edu.cn>
2025-01-19 20:12:05 +0800

    240119

--- train_script/train_direct.bash
+++ train_script/train_direct.bash
@@ -25,20 +25,22 @@ export NVIDIA_VISIBLE_DEVICES=1
 export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=power.draw,index --format=csv,nounits,noheader | sort -n | head -1 | awk '{ print $NF }')
 # export CUDA_VISIBLE_DEVICES=NUMBER_OF_GPU
 
-nohup bash -c '\
-	for cycle in {5..8}; do \
-		prev_cycle=$((cycle-1)); \
-		load_args=""; \
-		if [ $cycle -gt 1 ]; then \
-			load_args="--load cycle${prev_cycle} --load_epoch -10000"; \
-			~/anaconda3/envs/pyscf/bin/python test.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0 \
-				--precision float64 ${load_args} --density_restriction 1; \
-		else \
-			~/anaconda3/envs/pyscf/bin/python gen_data.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0; \
-		fi; \
-		~/anaconda3/envs/pyscf/bin/python train.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0 \
-			--eval_step 10 --epoch 29100 --with_eval False --precision float32 --save_dir cycle${cycle} \
-			--loss_multiplier 0.01 ${load_args}; \
-	done' >log/train-$$.log 2>&1 &
+mkdir -p log
+nohup bash <<'EOF' >log/train-$$.log 2>&1 &
+for cycle in {5..8}; do
+	prev_cycle=$((cycle-1))
+	load_args=""
+	if [ $cycle -gt 1 ]; then
+		load_args="--load cycle${prev_cycle} --load_epoch -10000"
+		~/anaconda3/envs/pyscf/bin/python test.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0 \
+			--precision float64 ${load_args} --density_restriction 1
+	else
+		~/anaconda3/envs/pyscf/bin/python gen_data.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0
+	fi
+	~/anaconda3/envs/pyscf/bin/python train.py -dl -0.5 0.5 3 --basis cc-pVDZ --extend_atom 0-1 --extend_xyz 0 \
+		--eval_step 10 --epoch 29100 --with_eval False --precision float32 --save_dir cycle${cycle} \
+		--loss_multiplier 0.01 ${load_args}
+done
+EOF
 
 echo $! >>log/save_pid.txt 2>&1
