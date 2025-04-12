ab0dbc3983865c98065d6b5ac1bee11cd5f2d6a4
czh <pb142060@mail.ustc.edu.cn>
2025-04-12 09:31:15 +0800

    250411

--- cc2cc/utils/parser.py
+++ cc2cc/utils/parser.py
@@ -359,6 +359,7 @@ def add_args(parser: argparse.ArgumentParser):
         args.extend_xyz[i] += 1
 
     args.distance_list = gen_logger(args.distance_list)
+    args.name_mol_input = args.name_mol.copy()
     args.name_mol = gen_name_args(args.name_mol, args)
 
     if args.train_atom not in periodic_table.keys():
