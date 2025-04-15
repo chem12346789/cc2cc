2d1e078b97490b77167cbfef2c878f46e2d62755
czh <pb142060@mail.ustc.edu.cn>
2025-04-15 16:11:56 +0800

    250415

--- train.py
+++ train.py
@@ -8,13 +8,32 @@ train_str_dict = [
     "molecule0",
     "molecule1",
     "molecule2-W4_11",
+    "molecule2-BSR36",
+    "molecule2-ALK8",
+    "molecule2-HEAVYSB11",
     "molecule3-W4_11",
-    "molecule4-W4_11",
+    "molecule3-ALK8",
+    "molecule3-HEAVYSB11",
+    "molecule4-ALK8",
+    "molecule5-BSR36",
+    "molecule5-ALK8",
+    # "molecule7-MB16_43",
+    # "molecule8-MB16_43",
+    # "ADIM6-AD2",
+    # "ADIM6-AD3",
+    # "molecule5-BSR36",
+    # "molecule6-BSR36",
+    # "molecule7-BSR36",
 ]
 eval_str_dict = [
+    "molecule4-W4_11",
     "molecule5-W4_11",
     "molecule5-BH9",
     "molecule5-BH76",
+    # "ADIM6-AD4",
+    # "molecule8-BSR36",
+    # "molecule8-IDISP",
+    # "molecule9-MB16_43",
 ]
 
 if __name__ == "__main__":
