3970157e27363dd164d1508b63818193cd65009b
czh <pb142060@mail.ustc.edu.cn>
2025-05-20 13:57:38 +0800

    250520

--- cc2cc/utils/TestData.py
+++ cc2cc/utils/TestData.py
@@ -111,6 +111,7 @@ class TestData:
             self.mf_dm1 = mf.make_rdm1()
             mycc = pyscf.cc.CCSD(mf)
             mycc.max_cycle = 200
+            mycc.direct = True
             mycc.set_frozen()
             print(f"Number of core orbital frozen: {mycc.frozen}")
         else:
@@ -180,7 +181,9 @@ class TestData:
             self.mf_dm1 = mf.make_rdm1()
             mycc = pyscf.cc.UCCSD(mf)
             mycc.max_cycle = 200
+            mycc.direct = True
             mycc.set_frozen()
+            print(f"Number of core orbital frozen: {mycc.frozen}")
         else:
             mf.kernel()
             self.mf_dm1 = mf.make_rdm1()
