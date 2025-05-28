727fad59092eea7b9b33cdab05f224303377772a
czh <pb142060@mail.ustc.edu.cn>
2025-05-29 02:50:04 +0800

    250529

--- cc2cc/utils/Grids.py
+++ cc2cc/utils/Grids.py
@@ -509,10 +509,10 @@ class Grid(dft.gen_grid.Grids):
             rho_4_lda = (rho_4[0][0], rho_4[1][0])
             rho_4_0 = rho_4[0][0] + rho_4[1][0]
 
-            e_lda, v_lda = ni.eval_xc_eff("LDA,", rho_4_lda, deriv=1, xctype="LDA")[1]
-            e_vwn, v_vwn = ni.eval_xc_eff(",VWN3", rho_4_lda, deriv=1, xctype="LDA")[1]
-            e_b88, v_b88 = ni.eval_xc_eff("B88,", rho, deriv=1, xctype="GGA")[1]
-            e_lyp, v_lyp = ni.eval_xc_eff(",LYP", rho, deriv=1, xctype="GGA")[1]
+            e_lda, v_lda = ni.eval_xc_eff("LDA,", rho_4_lda, deriv=1, xctype="LDA")[:2]
+            e_vwn, v_vwn = ni.eval_xc_eff(",VWN3", rho_4_lda, deriv=1, xctype="LDA")[:2]
+            e_b88, v_b88 = ni.eval_xc_eff("B88,", rho, deriv=1, xctype="GGA")[:2]
+            e_lyp, v_lyp = ni.eval_xc_eff(",LYP", rho, deriv=1, xctype="GGA")[:2]
 
             exc_b3lyp = 0.08 * e_lda + 0.19 * e_vwn + 0.72 * e_b88 + 0.81 * e_lyp
 
