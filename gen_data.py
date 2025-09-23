1fa2cab978f9ffd0c864846b1d01b25b381b8063
czh <pb142060@mail.ustc.edu.cn>
2025-09-24 03:57:26 +0800

    Add additional addon identifiers to train and eval lists; set wandb mode to offline

--- gen_data.py
+++ gen_data.py
@@ -16,6 +16,11 @@ train_str_list = [
     "ADDON_As",
     "ADDON_Ge",
     "ADDON_Se",
+    "ADDON_Te",
+    "ADDON_I",
+    "ADDON_Bi",
+    "ADDON_Pb",
+    "ADDON_Sb",
     "AHB21-1A",
     "AHB21-4A",
     "ALK8-li+",
@@ -815,13 +820,6 @@ if __name__ == "__main__":
     # remove the same name in eval and eval_str_exclude_list
     eval_str_list = [mol for mol in eval_str_list if mol not in eval_str_exclude_list]
 
-    name_mol_list, evaluate = [
-        "ADDON_Te",
-        "ADDON_I",
-        "ADDON_Bi",
-        "ADDON_Pb",
-        "ADDON_Sb",
-    ], False
     # name_mol_list, evaluate = train_str_list[2::3], False
     # name_mol_list, evaluate = eval_str_list[2::3], True
     error_molecule = []
