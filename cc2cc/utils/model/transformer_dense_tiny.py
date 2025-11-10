9c532213eee499dddd84eef32fe65ef666f73a49
czh <pb142060@mail.ustc.edu.cn>
2025-11-10 16:39:22 +0800

    Remove outdated transformer model implementations and add new dense transformer models with updated architecture.

--- cc2cc/utils/model/transformer_dense_tiny.py
+++ cc2cc/utils/model/transformer_dense_tiny.py
@@ -21,7 +21,7 @@ class Model(nn.Module):
         self.predictor = Extractor(
             d_model=CUBE_SIZE**3,
             seq_len=4,
-            num_layer=7,
+            num_layer=3,
             qkv_bias=False,
             num_heads=1,
             mlp_ratio=1,
@@ -32,7 +32,7 @@ class Model(nn.Module):
         self.densenet = DenseNet(
             d_model=4 * CUBE_SIZE**3,
             mlp=108,
-            depth=9,
+            depth=4,
             drop_rate=0,
             if_skip_connection_dense=1,
             dense_actv="gelu",
@@ -41,7 +41,7 @@ class Model(nn.Module):
         self.predictor_center = Extractor(
             d_model=1,
             seq_len=4,
-            num_layer=7,
+            num_layer=3,
             qkv_bias=False,
             num_heads=1,
             mlp_ratio=1,
@@ -51,8 +51,8 @@ class Model(nn.Module):
 
         self.densenet_center = DenseNet(
             d_model=4,
-            mlp=128,
-            depth=9,
+            mlp=108,
+            depth=4,
             if_skip_connection_dense=1,
             drop_rate=0,
             dense_actv="gelu",
