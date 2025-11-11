84279f0ffbb56a87463fa482d06200de33752dc3
czh <pb142060@mail.ustc.edu.cn>
2025-11-11 16:26:10 +0800

    Comment out unused molecule names in name_mol_list for clarity

--- gen_data.py
+++ gen_data.py
@@ -848,18 +848,18 @@ if __name__ == "__main__":
         name_mol_list, evaluate = train_str_list[args.training_cycle :: 3], False
 
     name_mol_list = [
-        "W4_11-ch2ch",
-        "W4_11-ch2nh2",
-        "W4_11-ch3nh",
-        "W4_11-ch3nh2",
+        # "W4_11-ch2ch",
+        # "W4_11-ch2nh2",
+        # "W4_11-ch3nh",
+        # "W4_11-ch3nh2",
         "W4_11-hoo",
         "W4_11-ssh",
         "W4_11-h2cn",
         "W4_11-hcnh",
-        "W4_11-sih3f",
-        "W4_11-b2h6",
-        "W4_11-c2h6",
-        "W4_11-si2h6",
+        # "W4_11-sih3f",
+        # "W4_11-b2h6",
+        # "W4_11-c2h6",
+        # "W4_11-si2h6",
     ]
     evaluate = False
 
