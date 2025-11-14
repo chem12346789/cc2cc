f0f870127a6b8e76b57e64dc92bdf3b47124840d
czh <pb142060@mail.ustc.edu.cn>
2025-11-14 21:34:32 +0800

    Refactor gen_data.py: Comment out existing molecule entries in train and eval lists

--- gen_data.py
+++ gen_data.py
@@ -13,15 +13,95 @@ from cc2cc.gen_ucc import ucc
 
 
 train_str_list = [
-    "molecule0-W4_11",
+    # "molecule0-W4_11",
+    # "AHB21-1A",
+    # "AHB21-4A",
+    # "ALK8-li+",
+    # "ALK8-na+",
+    # "ALKBDE10-ca",
+    # "ALKBDE10-k",
+    # "ALKBDE10-li",
+    # "ALKBDE10-mg",
+    # "ALKBDE10-na",
+    # "CHB6-24A",
+    # "DIPCS10-be_2+",
+    # "DIPCS10-mg_2+",
+    # "G21EA-EA_c-",
+    # "G21EA-EA_o-",
+    # "G21EA-EA_p-",
+    # "G21EA-EA_s-",
+    # "G21EA-EA_si-",
+    # "G21IP-al+",
+    # "G21IP-b+",
+    # "G21IP-be+",
+    # "G21IP-c+",
+    # "G21IP-cl+",
+    # "G21IP-f+",
+    # "G21IP-mg+",
+    # "G21IP-n+",
+    # "G21IP-o+",
+    # "G21IP-p+",
+    # "G21IP-s+",
+    # "G21IP-si+",
+    # "HEAVYSB11-br",
+    # "RG18-ar",
+    # "RG18-kr",
+    # "RG18-ne",
+    # "SIE4x4-he",
+    # "SIE4x4-he+",
+    # "molecule1-W4_11",
+    # "molecule2-W4_11",
 ]
 
 eval_str_list = [
-    "molecule1-W4_11",
-    "molecule2-W4_11",
-    "molecule3-W4_11",
-    "molecule4-W4_11",
-    "molecule5-W4_11",
+    # # #####################
+    # # ######  4(<9)  ######
+    # # #####################
+    # # "molecule4-W4_11",
+    # # #####################
+    # # ######  5(<9)  ######
+    # # #####################
+    # "molecule5-W4_11",
+    # # ######## 0 H ########
+    # "ALK8-li4_c",
+    # "G2RC-62",
+    # "G2RC-67",
+    # "HAL59-29_CF3Br-benB",
+    # "IL16-152B",
+    # "IL16-214B",
+    # "IL16-229B",
+    # # ######## 1 H ########
+    # "HAL59-BrBr_FCCH",
+    # "PNICO23-22b",
+    # # ######## 2 H ########
+    # "DC13-o3_c2h2_add",
+    # "RSE43-P5",
+    # "RSE43-P7",
+    # "TAUT15-7a",
+    # "TAUT15-7b",
+    # "YBDE18-nf3-ch2",
+    # "YBDE18-pf3-ch2",
+    # # ######## 3 H ########
+    # "IL16-230B",
+    # "RSE43-E5",
+    # "RSE43-E7",
+    # # #####################
+    # # ######  6(<9)  ######
+    # # #####################
+    # # ######## 0 H ########
+    # "RG18-ne6",
+    # # ######## 1 H ########
+    # "ALK8-li5_ch",
+    # "IL16-144B",
+    # "PArel-c2cl41",
+    # "PArel-c2cl42",
+    # "PArel-c2cl43",
+    # # ######## 2 H ########
+    # "RSE43-P28",
+    # # ######## 3 H ########
+    # "PArel-c2h2f41",
+    # "PArel-c2h2f42",
+    # "RSE43-E28",
 ]
 
 train_str_exclude_list = []
