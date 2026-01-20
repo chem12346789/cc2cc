fix(model): correct energy output calculation in DataBase

cc2cc/utils/DataBase.py
@@ -358,7 +358,9 @@ class DataBase:
                 output_mat = (data["exc_cc_grids"]) - (
                     data["exc_dft_grids"] + data["exc_k_dft_grids"]
                 )
-                output_mat *= energy_target / np.sum(output_mat * weight_mat)
+                corrected_energy = energy_target / np.sum(output_mat * weight_mat)
+                print(f"Corrected energy: {corrected_energy}")
+                output_mat *= corrected_energy
             else:
                 raise ValueError(
                     f"Unknown output target: {self.args.output_target}",

cc2cc/utils/model/transformer+dense_norm.py
@@ -62,7 +62,6 @@ class Model(nn.Module):
         x_norm_factor = torch.sum(
             torch.abs(x[:, :, :, :, :]),
             dim=(1, 2, 3, 4),
-            keepdim=True,
         )
         x_norm = torch.einsum(
             "x,x...->x...",
@@ -85,9 +84,8 @@ class Model(nn.Module):
         x_center = self.densenet_center(x_center)
 
         mixed_output = weight_out[:, [0]] * x_cube + weight_out[:, [1]] * x_center
-        mixed_output = torch.einsum(
+        return torch.einsum(
             "x,x...->x...",
             (x_norm_factor + ESP),
             mixed_output,
         )
-        return mixed_output * x_norm_factor