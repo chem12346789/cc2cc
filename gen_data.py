7001974b4a622b39e765d9beb03fb814cdb5d541
czh <pb142060@mail.ustc.edu.cn>
2026-01-27 13:54:49 +0800

    fix(gen_data): add molecules to skip for memory issues

--- gen_data.py
+++ gen_data.py
@@ -235,27 +235,12 @@ if __name__ == "__main__":
                 if (DATA_PATH / f"data_{name}.npz").exists():
                     continue
 
-            if name_mol in [
-                "W4_11-clo",
-                "W4_11-sif",
-                "W4_11-s2",
-                "W4_11-so",
-                "W4_11-t-hcoh",
-                "W4_11-nh2cl",
-                "W4_11-ch3nh",
-                "W4_11-ch2nh2",
-                "W4_11-ch3nh",
-                "W4_11-hcnh",
-                "W4_11-b2h6",
-            ]:
-                # these molecules experience memory issue in rdm calculation
-                raise RuntimeError("Skip due to memory issue in rdm calculation.")
-
             grids = Grid(mol, args.grid_level, 7)
             if mol.spin == 0:
                 cc(mol, grids, name, args, evaluate=evaluate)
             else:
-                ucc(mol, grids, name, args, evaluate=evaluate)
+                continue
+                # ucc(mol, grids, name, args, evaluate=evaluate)
         except (ValueError, RuntimeError) as e:
             print(f"ERROR: {name_mol} {args.md_number}")
             print(e)
