fix(DataBase): correct energy calculation and output

cc2cc/utils/ModelClass.py
@@ -315,11 +315,12 @@ class ModelClass:
         loss_record = np.abs((sum_target - torch.sum(output)).item())
 
         if if_train:
-            target = batch["output"] * weight
-            loss_abs_record = torch.sum(torch.abs(target - output)).item()
-            tot_loss += loss_multiplier_abs * self.loss_ene_abs(
-                data_weight * target, data_weight * output
-            )
+            if self.args.if_abs:
+                target = batch["output"] * weight
+                loss_abs_record = torch.sum(torch.abs(target - output)).item()
+                tot_loss += loss_multiplier_abs * self.loss_ene_abs(
+                    data_weight * target, data_weight * output
+                )
 
             if self.args.if_grad:
                 tot_loss += loss_multiplier_grad * self.loss_grad(grad_cc_train, force)