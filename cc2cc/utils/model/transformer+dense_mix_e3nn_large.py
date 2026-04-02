02eeb22722eb59716b0669da58e3ced098edd0f6
czh <pb142060@mail.ustc.edu.cn>
2026-04-02 15:54:40 +0800

    refactor: streamline E3nn model initialization and usage
    Consolidate E3nn layers into a ModuleList for better management and update dimensions in predictor.

--- cc2cc/utils/model/transformer+dense_mix_e3nn_large.py
+++ cc2cc/utils/model/transformer+dense_mix_e3nn_large.py
@@ -21,26 +21,28 @@ class Model(torch.nn.Module):
         self.cube_middle = (self.cube_size - 1) // 2
         self.input_level = 4
         self.before_weight = False
-        self.lmax = 2
-        self.out_l = 1
+        self.lmax = 4
+        self.out_l = 0
+        self.d_model = self.cube_size * (2 * self.out_l + 1)
+        self.e3nn_level = 8
 
-        self.conv1 = E3nn(
-            self.cube_type, self.cube_size, self.input_level, self.lmax, self.out_l
-        )
-        self.conv2 = E3nn(
-            self.cube_type, self.cube_size, self.input_level, self.lmax, self.out_l
-        )
-        self.conv3 = E3nn(
-            self.cube_type, self.cube_size, self.input_level, self.lmax, self.out_l
-        )
-        self.conv4 = E3nn(
-            self.cube_type, self.cube_size, self.input_level, self.lmax, self.out_l
+        self.e3nn = torch.nn.ModuleList(
+            [
+                E3nn(
+                    self.cube_type,
+                    self.cube_size,
+                    self.input_level,
+                    self.lmax,
+                    self.out_l,
+                )
+                for _ in range(self.e3nn_level)
+            ]
         )
 
         self.predictor = Transformer(
-            d_model=self.cube_size,
-            seq_len=self.input_level,
-            num_layer=5,
+            d_model=self.d_model,
+            seq_len=self.e3nn_level,
+            num_layer=7,
             qkv_bias=False,
             ffn_bias=False,
             mlp_ratio=1,
@@ -48,9 +50,9 @@ class Model(torch.nn.Module):
         )
 
         self.densenet = DenseNet(
-            d_model=self.input_level * self.cube_size,
+            d_model=self.e3nn_level * self.d_model,
             mlp=108,
-            depth=5,
+            depth=7,
             dense_bias=False,
             if_skip_connection_dense=0,
             dense_actv="gelu",
@@ -59,14 +61,14 @@ class Model(torch.nn.Module):
         self.densenet_center = DenseNet(
             d_model=self.input_level,
             mlp=108,
-            depth=5,
+            depth=7,
             dense_bias=False,
             if_skip_connection_dense=0,
             drop_rate=0,
             dense_actv="gelu",
         )
 
-        self.mixing_weight = torch.nn.Linear(self.input_level * self.cube_size, 6)
+        self.mixing_weight = torch.nn.Linear(self.e3nn_level * self.d_model, 6)
         self.weight_softmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, x):
@@ -76,20 +78,21 @@ class Model(torch.nn.Module):
         x_center = x[:, :, self.cube_middle]
 
         x_in = x.permute(0, 2, 1).contiguous()
-        out1 = torch.vmap(self.conv1)(x_in)
-        out2 = torch.vmap(self.conv2)(x_in)
-        out3 = torch.vmap(self.conv3)(x_in)
-        out4 = torch.vmap(self.conv4)(x_in)
-        x_cube = torch.cat([out1, out2, out3, out4], dim=-2)
+        x_cube = []
+        for e3nn_layer in self.e3nn:
+            x_out = torch.vmap(e3nn_layer)(x_in)
+            x_cube.append(x_out)
+        x_cube = torch.cat(x_cube, dim=-2)
+        print(f"x_cube shape: {x_cube.shape}")
 
         # do mixing x and x_center using Mixture of experts mechanism
         weight_out = self.mixing_weight(
-            x_cube.reshape(-1, self.input_level * self.cube_size)
+            x_cube.reshape(-1, self.e3nn_level * self.d_model)
         )
         weight_out = self.weight_softmax(weight_out)
 
         x_cube = self.predictor(x_cube)
-        x_cube = x_cube.reshape(-1, self.input_level * self.cube_size)
+        x_cube = x_cube.reshape(-1, self.e3nn_level * self.d_model)
         x_cube = self.densenet(x_cube)
 
         # # Extract the central values for each channel
