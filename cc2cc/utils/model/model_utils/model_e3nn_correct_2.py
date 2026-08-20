3854a374928f5aec971a68d331b041ca4260c4c9
czh <pb142060@mail.ustc.edu.cn>
2026-08-20 15:48:44 +0800

    fix: adjust hidden tensor summation to maintain dimensionality in E3nn forward pass

--- cc2cc/utils/model/model_utils/model_e3nn_correct_2.py
+++ cc2cc/utils/model/model_utils/model_e3nn_correct_2.py
@@ -79,8 +79,8 @@ class E3nn(torch.nn.Module):
         # f_in shape: [CUBE_SIZE**3, 4]
         f_hidden = self.tp1(f_in, self.sh)
         # f_hidden shape: [CUBE_SIZE**3, (lmax+1)**2]
-        f_hidden = f_hidden.sum(dim=0)
-        # f_hidden shape: [(lmax+1)**2]
+        f_hidden = f_hidden.sum(dim=-2, keepdim=True)
+        # f_hidden shape: [1, (lmax+1)**2]
         f_out = self.readout(f_hidden)
         # f_out shape: [1, CUBE_SIZE**3]
         return f_out
