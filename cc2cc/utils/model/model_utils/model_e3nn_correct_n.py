ebc11a65ef37a53a0d5639a3ba99685aa9a5180c
czh <pb142060@mail.ustc.edu.cn>
2026-08-27 17:40:54 +0800

    feat: refactor E3nn class and add testing functions for symmetry invariance

--- cc2cc/utils/model/model_utils/model_e3nn_correct_n.py
+++ cc2cc/utils/model/model_utils/model_e3nn_correct_n.py
@@ -1,5 +1,6 @@
 import torch
 from e3nn import o3
+from e3nn.nn import Gate
 
 from cc2cc.utils.env_var import EDGE_SIZE, EDGE_LEN, CUBE_MIDDLE
 
@@ -14,67 +15,52 @@ from cc2cc.utils.env_var import EDGE_SIZE, EDGE_LEN, CUBE_MIDDLE
 
 
 class E3nn(torch.nn.Module):
-    def __init__(
-        self,
-        cube_type="cube",
-        cube_size=27,
-        input_level=4,
-        lmax=2,
-        out_l=0,
-    ):
+    def __init__(self, cube_type, cube_size, input_level, lmax):
         super().__init__()
 
         self.input_level = input_level
         self.cube_type = cube_type
         self.cube_size = cube_size
-        self.out_l = out_l
         self.lmax = lmax
 
         if self.cube_type == "cube":
-            if torch.cuda.is_available():
-                edge_vec = torch.zeros(
-                    (EDGE_SIZE, EDGE_SIZE, EDGE_SIZE, 3),
-                    device="cuda",
-                    dtype=torch.float64,
-                )
-            else:
-                edge_vec = torch.zeros(
-                    (EDGE_SIZE, EDGE_SIZE, EDGE_SIZE, 3),
-                    device="cpu",
-                    dtype=torch.float64,
-                )
-            self.register_buffer("edge_vec", edge_vec, persistent=False)
+            edge_vec = torch.zeros(
+                (EDGE_SIZE, EDGE_SIZE, EDGE_SIZE, 3),
+                device="cpu",
+                dtype=torch.get_default_dtype(),
+            )
             for i in range(EDGE_SIZE):
                 for j in range(EDGE_SIZE):
                     for k in range(EDGE_SIZE):
-                        self.edge_vec[i, j, k, 0] = (i - CUBE_MIDDLE) * EDGE_LEN
-                        self.edge_vec[i, j, k, 1] = (j - CUBE_MIDDLE) * EDGE_LEN
-                        self.edge_vec[i, j, k, 2] = (k - CUBE_MIDDLE) * EDGE_LEN
+                        edge_vec[i, j, k, 0] = (i - CUBE_MIDDLE) * EDGE_LEN
+                        edge_vec[i, j, k, 1] = (j - CUBE_MIDDLE) * EDGE_LEN
+                        edge_vec[i, j, k, 2] = (k - CUBE_MIDDLE) * EDGE_LEN
         else:
             raise NotImplementedError("Only cube type is implemented.")
-        self.edge_vec = self.edge_vec.reshape(EDGE_SIZE**3, 3)
-
-        if torch.cuda.is_available():
-            center_vec = torch.zeros(1, 3, device="cuda", dtype=torch.float64)
-        else:
-            center_vec = torch.zeros(1, 3, device="cpu", dtype=torch.float64)
-        self.register_buffer("center_vec", center_vec, persistent=False)
-        self.center_vec = self.center_vec.reshape(1, 3)
+        edge_vec = edge_vec.reshape(EDGE_SIZE**3, 3)
 
         irreps_input = o3.Irreps(f"{self.input_level}x0e")
+        # hidden_irreps = o3.Irreps(
+        #     "+".join([f"{self.input_level}x{i}e" for i in range(self.lmax + 1)])
+        # )
         hidden_irreps = o3.Irreps(
-            "+".join([f"{self.input_level}x{i}e" for i in range(self.lmax + 1)])
+            "+".join(
+                [
+                    f"{self.input_level}x{i}{'e' if i % 2 == 0 else 'o'}"
+                    for i in range(self.lmax + 1)
+                ]
+            )
         )
-        irreps_output = o3.Irreps(f"{self.cube_size}x{self.out_l}e")
+        irreps_output = o3.Irreps(f"{self.cube_size}x{0}e")
 
         irreps_sh = o3.Irreps.spherical_harmonics(lmax=self.lmax)
-        self.sh = o3.spherical_harmonics(
+        sh = o3.spherical_harmonics(
             irreps_sh,
-            self.edge_vec,
+            edge_vec,
             normalize=True,
             normalization="component",
         )
-        self.tp = o3.FullyConnectedTensorProduct(
+        self.tp1 = o3.FullyConnectedTensorProduct(
             irreps_input,
             irreps_sh,
             hidden_irreps,
@@ -82,32 +68,82 @@ class E3nn(torch.nn.Module):
             internal_weights=True,
         )
 
-        irreps_sh_center = o3.Irreps.spherical_harmonics(lmax=self.lmax)
-        self.sh_center = o3.spherical_harmonics(
-            irreps_sh_center,
-            self.center_vec,
-            normalize=True,
-            normalization="component",
-        )
-        self.tp_center = o3.FullyConnectedTensorProduct(
-            hidden_irreps,
-            irreps_sh_center,
-            irreps_output,
-            shared_weights=True,
-            internal_weights=True,
-        )
+        self.readout = o3.Linear(hidden_irreps, irreps_output)
 
-        # xavier_uniform_ initialization for the tensor product weights
+        # uniform_ initialization for the tensor product weights
         with torch.no_grad():
-            self.tp.weight.uniform_(-1, 1)
-            self.tp_center.weight.uniform_(-1, 1)
+            self.tp1.weight.uniform_(-1, 1)
+            self.readout.weight.uniform_(-1, 1)
+
+        self.register_buffer("edge_vec", edge_vec, persistent=False)
+        self.register_buffer("sh", sh, persistent=False)
 
     def forward(self, f_in):
         # f_in shape: [CUBE_SIZE**3, 4]
-        f_hidden = self.tp(f_in, self.sh)
+        f_hidden = self.tp1(f_in, self.sh)
         # f_hidden shape: [CUBE_SIZE**3, (lmax+1)**2]
-        f_hidden = f_hidden.sum(dim=0)
-        # f_hidden shape: [(lmax+1)**2]
-        f_out = self.tp_center(f_hidden, self.sh_center)
+        f_hidden = f_hidden.sum(dim=-2, keepdim=True)
+        # f_hidden shape: [1, (lmax+1)**2]
+        f_out = self.readout(f_hidden)
         # f_out shape: [1, CUBE_SIZE**3]
         return f_out
+
+
+@torch.no_grad()
+def test_x_reflection(model):
+    model.eval()
+
+    x = torch.randn(
+        EDGE_SIZE,
+        EDGE_SIZE,
+        EDGE_SIZE,
+        model.input_level,
+        device=model.sh.device,
+        dtype=model.sh.dtype,
+    )
+
+    x_reflected = torch.flip(x, dims=[0])
+
+    y1 = model(x.reshape(EDGE_SIZE**3, -1))
+    y2 = model(x_reflected.reshape(EDGE_SIZE**3, -1))
+
+    error = (y1 - y2).abs().max()
+
+    print("Maximum invariant error:", error.item())
+
+    assert torch.allclose(
+        y1,
+        y2,
+        atol=1e-5,
+        rtol=1e-5,
+    )
+
+
+@torch.no_grad()
+def test_xy_swap(model):
+    model.eval()
+
+    x = torch.randn(
+        EDGE_SIZE,
+        EDGE_SIZE,
+        EDGE_SIZE,
+        model.input_level,
+        device=model.sh.device,
+        dtype=model.sh.dtype,
+    )
+
+    x_swapped = x.transpose(0, 1)
+
+    y1 = model(x.reshape(EDGE_SIZE**3, -1))
+    y2 = model(x_swapped.reshape(EDGE_SIZE**3, -1))
+
+    error = (y1 - y2).abs().max()
+
+    print("Maximum invariant error:", error.item())
+
+    assert torch.allclose(
+        y1,
+        y2,
+        atol=1e-5,
+        rtol=1e-5,
+    )
